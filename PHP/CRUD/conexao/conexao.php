50852797ef23e7aed4c26c07e42655edfdd66292
PedroMiho <pedromihosoza@gmail.com>
2025-05-08 10:58:42 -0300

    .

---
+++ PHP/CRUD/conexao/conexao.php
@@ -0,0 +1,19 @@
+<?php
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "faculdade";
+
+//Criando Conexão
+$conn = new mysqli($serverName, $userName, $password, $dbName);
+
+//Validação de Conexão
+if ($conn->connect_error){
+    echo "Conexão Falhou";
+}
+else {
+    echo "Conexão feita com sucesso";
+}
+
+
+?>
